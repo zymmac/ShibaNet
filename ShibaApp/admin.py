305591d102422a8e7f09bc699c50b9a2d7df3516
Alexandre Costa <zymmac@gmail.com>
2020-04-17 00:48:57 -0300

    Created ShibaUser model

--- ShibaApp/admin.py
+++ ShibaApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from ShibaApp.models import ShibaUser
 
 # Register your models here.
+admin.site.register(ShibaUser)
--- ShibaApp/models.py
+++ ShibaApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django import forms
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ShibaUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    profile_bio = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.user.username
